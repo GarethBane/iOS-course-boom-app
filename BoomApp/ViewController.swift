e78519012074dc4b5c5b182ff1ba0603ab3750c3
John Doe <phillipbenford@Johndoe.local>
2015-11-24 20:46:14 +0000

    Added files for boomApp

--- BoomApp/ViewController.swift
+++ BoomApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var boomAppLogo: UIImageView!
+    @IBOutlet weak var boomAppBG: UIImageView!
+    @IBOutlet weak var BoomAppButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeCoolButton(sender: AnyObject) {
+        boomAppBG.hidden = false
+        boomAppLogo.hidden = false
+        BoomAppButton.hidden = true
+    }
 
 }
 
